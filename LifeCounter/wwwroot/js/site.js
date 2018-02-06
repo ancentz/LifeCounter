20b945f5618e69a628934064aa48d7f75630bdca
ancentz <ancentz@gmail.com>
2018-02-06 13:59:10 +1100

    initial commit

---
+++ LifeCounter/wwwroot/js/site.js
@@ -0,0 +1,54 @@
+﻿// Write your Javascript code.
+function generatePlayerLifeView() {
+    var playerNamesContainer = document.getElementById("playerNamesContainer");
+    var playerHealthContainer = document.getElementById("playerHealthContainer");
+
+    hideElements("players");
+    hideElements("health");
+    hideElements("okButton");
+    
+    for (var j = 2; j > 0; j--) {
+        for (var i = 0; i < document.getElementById("players").value; i++) {
+            var newElement = document.createElement("input");
+            if (j == 2) {
+                newElement.setAttribute("id", j + "" + i);
+                newElement.setAttribute("class", "form-control");
+                playerNamesContainer.append(document.createElement("p"));
+                playerNamesContainer.append(newElement);
+            }
+            else {
+                newElement.setAttribute("id", i);
+                newElement.setAttribute("class", "form-control");
+                newElement.value = document.getElementById("health").value;
+                newElement.addEventListener('keypress', function (e) {
+                    if (e.keyCode === 13) {
+                        console.log(e.target.value, e.target.id);
+                        calcuateLifeTotal(e.target.value, e.target.id);
+                    }
+                });
+                playerHealthContainer.append(document.createElement("p"));
+                playerHealthContainer.append(newElement);
+            }
+        }
+    }
+}
+
+function calcuateLifeTotal(test, id) {
+    var res = test.split("-");
+    var res2 = test.split("+");
+
+    if (res.length > 1) {
+        console.log(res);
+        document.getElementById(id).value = res[0] - res[1];
+    }
+    
+    if (res2.length > 1) {
+        console.log(res2);
+
+        document.getElementById(id).value = parseInt(res2[0]) + parseInt(res2[1]) ;
+    }
+}
+
+function hideElements(id) {
+    document.getElementById(id).style.display = "none";
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+LifeCounter is a web application that can beused in multi-player games to track the lives of all players
+# LifeCounter
